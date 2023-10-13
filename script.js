9ced60b61e7354e81057d13c32efdaad29c6ad5c
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-13 12:22:20 +0545

    completing animations

--- script.js
+++ script.js
@@ -24,3 +24,35 @@ Shery.imageEffect("#back", {
   },
   gooey: true,
 });
+
+var elems = document.querySelectorAll(".elem");
+
+elems.forEach(function (elem) {
+  var h1s = elem.querySelectorAll("h1");
+  var index = 0;
+  var animating=false;
+
+  document.querySelector("#main").addEventListener("click", function () {
+    if(!animating){
+      animating=true;
+
+      gsap.to(h1s[index], {
+        top: "-=100%",
+        ease: Expo.easeInOut,
+        duration: 1,
+        onComplete: function () {
+          gsap.set(this._targets[0], { top: "100%" });
+          animating=false;
+        },
+      });
+  
+      index === h1s.length - 1 ? (index = 0) : index++;
+  
+      gsap.to(h1s[index], {
+        top: "-=100%",
+        ease: Expo.easeInOut,
+        duration: 1,
+      });
+    }
+  });
+});
